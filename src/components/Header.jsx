8f2880dd5093cf1d04a1f8d641d05013d261d629
AgustinSegui <agussegui24@hotmail.es>
2024-02-29 00:33:45 -0300

    Terminar el editado de los gastos

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import ControlPresupuesto from "./ControlPresupuesto"
+import NuevoPresupuesto from "./NuevoPresupuesto"
+
+
+const Header = ({
+  gastos,
+  presupuesto, 
+  setPresupuesto,
+  isValidPresupuesto,
+  setIsValidPresupuesto
+}) => {
+  return (
+    <header>
+        <h1>Planificador de Gastos</h1>
+
+        {isValidPresupuesto ? (
+          <ControlPresupuesto
+            gastos={gastos}
+            presupuesto={presupuesto}
+          />
+        ) : (
+          <NuevoPresupuesto 
+            presupuesto={presupuesto}
+            setPresupuesto={setPresupuesto} 
+            setIsValidPresupuesto={setIsValidPresupuesto} 
+          />
+        )}
+        
+    </header>
+  )
+}
+
+export default Header
